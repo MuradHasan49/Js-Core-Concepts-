a6a7f65a7463873a953668a7a58a6666525adfc6
Murad Hasan <muradhasanshuco00@gmail.com>
2026-03-03 00:37:45 +0600

    explor equals

---
+++ equals.js
@@ -0,0 +1,5 @@
+
+console.log(2===2);
+console.log(2===3);
+console.log(2==='2');
+// compares the data type first. if data types are different, it returns false. if data types are same, it compares the values and returns true or false accordingly.
